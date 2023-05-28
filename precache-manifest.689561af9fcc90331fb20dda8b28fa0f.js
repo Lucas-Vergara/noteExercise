7ed83cc2e2a4d0286cf5e99ce947a600596529bf
Lucas-Stratagemma <123574112+Lucas-Stratagemma@users.noreply.github.com>
2023-05-28 19:36:35 -0400

    Updates

---
+++ precache-manifest.689561af9fcc90331fb20dda8b28fa0f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "2184ec8f147ee75cea49",
+    "url": "/Lucas-Vergara/notesExercise/static/css/main.767dab06.chunk.css"
+  },
+  {
+    "revision": "2184ec8f147ee75cea49",
+    "url": "/Lucas-Vergara/notesExercise/static/js/main.2184ec8f.chunk.js"
+  },
+  {
+    "revision": "3af03192f83efab4825f",
+    "url": "/Lucas-Vergara/notesExercise/static/js/1.3af03192.chunk.js"
+  },
+  {
+    "revision": "032d0095d2008708d7b5",
+    "url": "/Lucas-Vergara/notesExercise/static/js/2.032d0095.chunk.js"
+  },
+  {
+    "revision": "1f7a35c25fd7f6ecd2b6",
+    "url": "/Lucas-Vergara/notesExercise/static/js/runtime~main.1f7a35c2.js"
+  },
+  {
+    "revision": "51e4d1d1fc836d4b2505dde4d975cd77",
+    "url": "/Lucas-Vergara/notesExercise/index.html"
+  }
+];
